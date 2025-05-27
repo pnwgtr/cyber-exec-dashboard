15e4d7d51d32f917c2c66f6c0c4eb4433085179f
pnwgtr <117317945+pnwgtr@users.noreply.github.com>
2025-05-26 22:58:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,14 @@ div[data-testid="stMetric-value"] {
     text-align: center !important;
     font-weight: 800;
 }
+
+/* Center the entire metric container */
+div[data-testid="stMetric"] > div {
+    display: flex !important;
+    flex-direction: column !important;
+    align-items: center !important;
+    justify-content: center !important;
+}
 </style>
     """,
     unsafe_allow_html=True,
